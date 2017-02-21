63c93e162118cc0460624299820ab0083c4dd7ce
Raditya <arya.raditya26@gmail.com>
2017-02-21 12:57:45 +0700

    added model

---
+++ server/model/house.model.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const mongoose = require('mongoose');
+var Schema     = mongoose.Schema;
+
+var houseSchema = new Schema({
+  title     : String,
+  luasTanah : String,
+  harga     : Number,
+  lokasi    : String,
+  fasilitas : String,
+  lainnya   : String,
+  gambar    : String
+},
+{
+  timestamps : true
+})
+
+var Houses = mongoose.model('Houses', houseSchema)
+
+module.exports = Houses;
